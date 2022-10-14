0073097492605566e840221b3d762a07c2e88393
xDamneDx <xdamnedx@gmail.com>
2022-10-14 17:21:10 +0300

    Completed

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,11 +21,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function minesweeper(matrix) {
+  const blankMatrix = matrix.map((line) => line.map((_) => 0));
+
+  blankMatrix.map((line, lineIndex) =>
+    line.map((_, cellIndex) => {
+      if (matrix[lineIndex][cellIndex]) {
+        for (let li = lineIndex - 1; li <= lineIndex + 1; li += 1) {
+          if (li > -1) {
+            for (let ci = cellIndex - 1; ci <= cellIndex + 1; ci += 1) {
+              if (ci > -1 && !(li === lineIndex && ci === cellIndex)) {
+                blankMatrix[li][ci] += 1;
+              }
+            }
+          }
+        }
+      }
+    })
+  );
+
+  return blankMatrix;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
